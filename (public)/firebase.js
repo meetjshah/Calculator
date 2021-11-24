556e1263573d55a0c6b6746910b7312e7befe643
meetshah <meetshah@iu.edu>
2021-11-24 03:00:24 -0500

    Firebase Hosting

---
+++ (public)/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6Oxa6dSbTBlfqfyO5AuYNXoQCtzNwo5U",
+  authDomain: "calculator-meet.firebaseapp.com",
+  projectId: "calculator-meet",
+  storageBucket: "calculator-meet.appspot.com",
+  messagingSenderId: "155013372352",
+  appId: "1:155013372352:web:b6cfe8ff737623e5a51025"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
